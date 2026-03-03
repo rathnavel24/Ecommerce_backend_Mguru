62d9c5ce56f5be1a9e3690c6eb6702362565ca68
Koushika10 <koushikrishna1028@gmail.com>
2026-03-03 17:34:55 +0530

    Merg conflict

--- app/app/db/database.py
+++ app/app/db/database.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-print("this is DB")
-=======
 print("This is DB")
->>>>>>> 07ef514caa99d10d4fbf24f3c623c697334409d0
--- app/app/models/products.py
+++ app/app/models/products.py
@@ -1,4 +1,2 @@
-<<<<<<< HEAD
-=======
+
 print ("This is ppppp")
->>>>>>> 07ef514caa99d10d4fbf24f3c623c697334409d0
