693fa6d69ea1985131b329deddfd35110e42d3da
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-04 23:11:21 +0530

    Login crud change

--- backend/app/app/crud/login_crud.py
+++ backend/app/app/crud/login_crud.py
@@ -1,1 +1,19 @@
-#write login logic
+from fastapi import HTTPException
+from starlette import status
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+from abc import ABC,abstractmethod
+
+class LoginAbstract(ABC):
+    @abstractmethod
+    def user_validation():
+        pass
+    @abstractmethod
+    def token_generation():
+        pass
+
+    
+class LoginDetails(LoginAbstract):
+    def __init__(self,db:Session, user_data):
+        self.db = db
+        self.user_data = user_data
