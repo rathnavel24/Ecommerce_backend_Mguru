2b27560201a631afdd138a6682ba45baa00e835e
pdharshini1905-lang <pdharshini1905@gmail.com>
2026-03-04 15:05:38 +0530

    priyadharshini db modules

--- app/app/models/ecommerce_token.py
+++ app/app/models/ecommerce_token.py
@@ -1,6 +1,14 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column, String, TEXT, SmallInteger, TIMESTAMP, Integer, Boolean, func, ForeignKey
 from sqlalchemy.orm import relationship
 
 class EcommerceToken(Base):
     __tablename__ = 'token'
+
+    token_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.user_id"))
+    hashed_token = Column(String(255))
+    expiresat = Column(TIMESTAMP, default=func.now())
+    created_at = Column(TIMESTAMP, default=func.now())
+
+    user = relationship("ecommerce_user", back_populates="ecom_user")
