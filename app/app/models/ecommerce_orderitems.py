9684aac03ecb4f04117457c6fb62ca0ef37e2189
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-04 14:17:16 +0530

    db Schema Done

--- app/app/models/ecommerce_orderitems.py
+++ app/app/models/ecommerce_orderitems.py
@@ -1,6 +1,16 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column,String,TIMESTAMP,Integer,func,DECIMAL,Enum
 from sqlalchemy.orm import relationship
 
 class EcommerceOrderItems(Base):
     __tablename__ = 'order_items'
+    orderitems_id = Column(Integer,primary_key=True)
+    order_id = Column(Integer)
+    product_id = Column(Integer)
+    product_name = Column(String(100))
+    price = Column(DECIMAL(10,2))
+    quantity = Column(Integer)
+    status = Column(Enum('active','inactive','deleted'))
+    createdat = Column(TIMESTAMP,default= func.now())
+    updatedat = Column(TIMESTAMP,default=None,onupdate=func.now())
+    createdby = Column(String(50),default='ADMIN')
--- app/app/models/ecommerce_productinfo.py
+++ app/app/models/ecommerce_productinfo.py
@@ -1,6 +1,18 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column,String,TEXT,TIMESTAMP,Integer,func,DECIMAL,Enum
 from sqlalchemy.orm import relationship
 
 class EcommerceProductInfo(Base):
     __tablename__ = 'product_info'
+    product_id = Column(Integer,primary_key=True)
+    product_name = Column(String(255))
+    categorie_id = Column(Integer)
+    price  = Column(DECIMAL(10,2))
+    discount_percent = Column(DECIMAL(10,2))
+    description = Column(TEXT)
+    image_url = Column(TEXT)
+    sku = Column(String(100))
+    status = Column(Enum('active','inactive','deleted'))
+    createdat = Column(TIMESTAMP,default= func.now())
+    updatedat = Column(TIMESTAMP,default=None,onupdate=func.now())
+    createdby = Column(String(50),default='ADMIN')
