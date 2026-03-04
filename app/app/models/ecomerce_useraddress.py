e2323cd77ca0ebb6e63e26396db30774bc51b26c
Koushika10 <koushikrishna1028@gmail.com>
2026-03-04 13:54:16 +0530

    address update

--- app/app/models/ecomerce_useraddress.py
+++ app/app/models/ecomerce_useraddress.py
@@ -1,6 +1,16 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column,String,TEXT,Integer,Boolean,ForeignKey
 from sqlalchemy.orm import relationship
 
 class EcommerceUserAddress(Base):
     __tablename__ = 'user_address'
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer,ForeignKey("User.user_id"))
+    address_line1 = Column(TEXT)
+    address_line2 = Column(TEXT)
+    city = Column(String(50))
+    state = Column(String(50))
+    country = Column(String(50))
+    pincode = Column(String(10))
+    isdefault = Column(Boolean)
