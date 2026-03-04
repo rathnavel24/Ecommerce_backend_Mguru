d3194443c325f1eda60ac97c7d971c86e65ca6aa
Koushika10 <koushikrishna1028@gmail.com>
2026-03-04 14:23:47 +0530

    cart update

--- app/app/models/ecommerce_cart.py
+++ app/app/models/ecommerce_cart.py
@@ -1,6 +1,16 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column,String,TIMESTAMP,Integer,func,ForeignKey,Enum
 from sqlalchemy.orm import relationship
 
 class EcommerceCart(Base):
     __tablename__ = 'cart'
+
+    card_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.user_id"))
+    product_id = Column(Integer,ForeignKey("product_info.product_id"))
+    quantity = Column(Integer)
+    status = Column(Enum("active","inactive","deleted"))
+    created_at = Column(TIMESTAMP, default=func.now())
+    update_at = Column(TIMESTAMP, default=func.now())
+    created_by = Column(String(100))
+
