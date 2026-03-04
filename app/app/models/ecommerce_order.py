ceeefb6d9030a500ba08174ee5ff312455a6f331
Koushika10 <koushikrishna1028@gmail.com>
2026-03-04 14:56:59 +0530

    order update

--- app/app/models/ecommerce_order.py
+++ app/app/models/ecommerce_order.py
@@ -1,6 +1,16 @@
 from app.app.db.base import Base
-from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func
+from sqlalchemy import Column,String,TEXT,SmallInteger,TIMESTAMP,Integer,Boolean,func,ForeignKey,DECIMAL,Enum
 from sqlalchemy.orm import relationship
 
 class EcommerceOrder(Base):
     __tablename__ = 'order'
+
+    order_id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey("User.user_id"))
+    total_price = Column(DECIMAL(10,2))
+    shipping_address = Column(Integer)
+    order_status = Column(Enum("placed","shipped","delivered","cancelled"))
+    status = Column(Enum("active","inactive","deleted"))
+    created_at = Column(TIMESTAMP,default=func.now())
+    updated_at = Column(TIMESTAMP,default=func.now())
+    created_by = Column(String(100))
