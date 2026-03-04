143e3e6752269775ff4d9e66aff7aec834f6ff0b
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-04 11:45:45 +0530

    Db connection string

--- app/app/db/session.py
+++ app/app/db/session.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-conn = "mysql+pymysql://root:1234@localhost/ecommerce"
+conn = "mysql+pymysql://2UzLbM74cdpvQ6S.root:1OqTxHRkMOc0H6ie@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/ecommerce"
 
-engine = create_engine(conn)
+engine = create_engine(
+    conn,
+    connect_args={
+        "ssl": {
+            "ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
 
 sessionLocal = sessionmaker(bind=engine)
