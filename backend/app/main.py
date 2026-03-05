2d71bf8eaa13542efe4fdd884cfbc3b7186658d7
pdharshini1905-lang <pdharshini1905@gmail.com>
2026-03-05 16:42:30 +0530

    Changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.app.api.endpoints import login
+from app.app.crud.login_crud import reset_password
 from app.app.db.init_db import init_db
 
 app = FastAPI()
 
 init_db()
 app.include_router(login.router)
+
+init_db()
+app.include_router(reset_password.router)
