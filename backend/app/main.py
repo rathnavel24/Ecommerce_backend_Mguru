8b2d9c48e58fcce7a72f12c5fe5c0e841389f9b0
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-05 13:27:02 +0530

    signup page

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.app.api.endpoints import login
+from app.app.api.endpoints import signup
 from app.app.db.init_db import init_db
 
 app = FastAPI()
 
-init_db()
+#init_db()
+
+app.include_router(signup.router)
 app.include_router(login.router)
