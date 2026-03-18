7d6eee467a686eef4ce88310011d6ef6fc78d507
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-18 17:57:06 +0530

    smtp disabled

--- backend/app/app/crud/otp_crud.py
+++ backend/app/app/crud/otp_crud.py
@@ -1,5 +1,8 @@
-import resend
 
+#import smtplib
+import resend
+#from sqlalchemy.orm import Session
+#from email.message import EmailMessage
 
 def otp_sent(email,otp):
 
@@ -18,6 +21,23 @@ def otp_sent(email,otp):
             }
     except Exception as e:
         return e
+    
+    '''
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('rathnavelwork@gmail.com','sjxo fcnt jiww crtm')
+    to_mail = email
+
+    msg = EmailMessage()
+    msg['Subject'] = "OTP Verification"
+    msg['From'] = 'rathnavelwork@gmail.com'
+    msg['To'] = to_mail
+    msg.set_content('Your_otp_is: '+otp)
+
+    server.send_message(msg)
+    server.quit()
+    return {"msg": "OTP sent"}
+    '''
 
 
 
